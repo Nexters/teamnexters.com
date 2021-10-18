b3183f2dc847cad8353e0a6b827fcfdff27dffe5
HunJin Kim <ysg01129@naver.com>
2021-10-18 21:38:18 +0900

    fix(develop): revert develop 브랜치 with git action
    - git action pipe-line at _content
    - dump folder makes

---
+++ .script/make_projects.py
@@ -0,0 +1,35 @@
+#!/bin/usr/python3.9
+import json
+
+with open("./_content/project.json", mode="r") as f:
+    google_results = f.read()
+
+_projects = json.loads(google_results)
+rows = _projects["results"][0]["result"]["rawData"]
+
+projects = []
+# 첫 행은 header이므로 제외
+for idx, row in enumerate(rows[1:]):
+    app_name, thumbnail, th, year, team_name, _members, description, ppt, android_link, ios_link, web_link = row
+    projects.append({
+        "idx": idx,
+        "app_name": app_name,
+        "thumbnail": thumbnail,
+        "th": th,
+        "year": year,
+        "team_name": team_name,
+        "members": [member.strip() for member in _members.split(",")],
+        "description": description,
+        "ppt": ppt,
+        "android_link": android_link,
+        "ios_link": ios_link,
+        "web_link": web_link
+    })
+
+for idx, project in enumerate(projects):
+    with open(f"./_content/projects/{idx}.json", mode="w", encoding="utf-8") as f:
+        f.write(json.dumps(project, ensure_ascii=False, indent=2))
+
+# # content/_projects 폴더를 생성하고, 폴더 안에 파일 쓰기
+
+# with open("./content/_projects/")
