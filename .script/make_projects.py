4578935be0b2ca48eaa3448093f947ef66d80597
Deo Kim <nexters@kakao.com>
2021-10-23 16:15:46 +0900

    feat(google-sheet): json 파일 생성 스크립트 추가 (#24)
    * feat(google-sheet): json 파일 생성 스크립트 추가
    
    Co-authored-by: GitHub Action <action@github.com>

--- .script/make_projects.py
+++ .script/make_projects.py
@@ -1,7 +1,7 @@
 #!/bin/usr/python3.9
 import json
 
-with open("./_content/project.json", mode="r") as f:
+with open("./content/project.json", mode="r") as f:
     google_results = f.read()
 
 _projects = json.loads(google_results)
@@ -10,7 +10,12 @@ rows = _projects["results"][0]["result"]["rawData"]
 projects = []
 # 첫 행은 header이므로 제외
 for idx, row in enumerate(rows[1:]):
-    app_name, thumbnail, th, year, team_name, _members, description, ppt, android_link, ios_link, web_link = row
+    app_name, _thumbnail, th, year, team_name, _members, description, ppt, android_link, ios_link, web_link = row
+    thumbnail = (
+        _thumbnail 
+        if f"https://drive.google.com/uc?export=view&id={_thumbnail}" 
+        else ""
+    )
     projects.append({
         "idx": idx,
         "app_name": app_name,
@@ -27,7 +32,7 @@ for idx, row in enumerate(rows[1:]):
     })
 
 for idx, project in enumerate(projects):
-    with open(f"./_content/projects/{idx}.json", mode="w", encoding="utf-8") as f:
+    with open(f"./content/projects/{idx}.json", mode="w", encoding="utf-8") as f:
         f.write(json.dumps(project, ensure_ascii=False, indent=2))
 
 # # content/_projects 폴더를 생성하고, 폴더 안에 파일 쓰기
