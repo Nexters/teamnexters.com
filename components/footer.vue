36a5bea9fbc4ada21ed4f660ecbd887ed932ca8f
HunJin Kim <ysg01129@naver.com>
2021-10-23 19:51:38 +0900

    feat(footer): footer에 스폰서 추가

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,30 @@
 <template>
   <footer>
     <div class="footer">
-      <div class="sns">
-        <ul v-for="item in items" :key="item.name">
-          <a :href="item.href">
-            <img :src="sns_icon[item.name]" :alt="item.name" />
-          </a>
-        </ul>
+      <div class="leftArea">
+        <div class="sns">
+          <ul v-for="item in items" :key="item.name">
+            <a :href="item.href">
+              <img :src="sns_icon[item.name]" :alt="item.name" />
+            </a>
+          </ul>
+        </div>
+        <div class="sponsor">
+          <p @click="handleSponsorClick">
+            sponsored
+            <img class="arrow-right" :src="arrowIcon" />
+          </p>
+          <div v-if="isSponsorVisible" class="sponsorBox">
+            <article class="sponsorList">
+              <section class="sponsorItem">
+                <span>Naver Cloud Platform</span>
+              </section>
+              <section class="sponsorItem">
+                <span>Naver D2 </span>
+              </section>
+            </article>
+          </div>
+        </div>
       </div>
       <div class="copyrights">
         {{ copyrights }}
@@ -48,13 +66,27 @@ export default defineComponent({
           ? require("~/assets/img/instagram.svg")
           : require("~/assets/img/instagram_black.svg"),
       },
+      arrowIcon: props.isWhite
+        ? require("~/assets/img/arrow_right.svg")
+        : require("~/assets/img/arrow_right_black.svg"),
     };
+  },
+  data() {
+    return {
+      isSponsorVisible: false,
+    };
+  },
+  methods: {
+    handleSponsorClick() {
+      this.isSponsorVisible = !this.isSponsorVisible;
+    },
   },
 });
 </script>
 
 <style lang="scss" scoped>
 @import "~/assets/css/_device.scss";
+@import "~/assets/css/setting.scss";
 
 * {
   font-family: Spoqa Han Sans Neo;
@@ -65,6 +97,51 @@ footer {
 }
 
 .footer {
+  .sponsor {
+    position: relative;
+    > p {
+      display: flex;
+      align-items: center;
+
+      align-self: flex-end;
+      font-style: normal;
+      font-weight: normal;
+      letter-spacing: -0.02em;
+      opacity: 0.6;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+    .arrow-right {
+      width: 16px;
+      height: 16px;
+    }
+    .sponsorBox {
+      position: absolute;
+      bottom: 0;
+      padding: 16px;
+      border: 1px solid $grey02;
+      border-radius: 8px;
+      background-color: $white;
+
+      .sponsorList {
+        .sponsorItem {
+          font-weight: 400;
+          letter-spacing: -0.2;
+          color: $grey03;
+        }
+      }
+    }
+  }
+  .copyrights {
+    align-self: flex-end;
+    font-style: normal;
+    font-weight: normal;
+    text-align: center;
+    letter-spacing: -0.02em;
+    opacity: 0.6;
+  }
   @include mobile {
     display: flex;
     margin-left: auto;
@@ -73,6 +150,7 @@ footer {
     .sns {
       display: flex;
       margin-right: auto;
+      margin-bottom: 16px;
       img {
         height: 24px;
         width: 24px;
@@ -83,17 +161,26 @@ footer {
         margin-right: 16px;
       }
     }
+    .sponsor {
+      > p {
+        font-size: 14px;
+        line-height: 21px;
+      }
+    }
     .copyrights {
       margin-right: auto;
       padding-top: 16px;
 
-      font-style: normal;
-      font-weight: normal;
       font-size: 14px;
       line-height: 21px;
-      text-align: center;
-      letter-spacing: -0.02em;
-      opacity: 0.6;
+    }
+    .sponsorBox {
+      right: -60px;
+
+      .sponsorItem {
+        font-size: 12px;
+        line-height: 18px;
+      }
     }
   }
   @include tablet {
@@ -106,6 +193,7 @@ footer {
 
     .sns {
       display: flex;
+      margin-bottom: 24px;
       img {
         height: 32px;
         width: 32px;
@@ -116,16 +204,25 @@ footer {
         margin-right: 32px;
       }
     }
+    .sponsor {
+      > p {
+        font-size: 16px;
+        line-height: 24px;
+      }
+    }
     .copyrights {
       margin-left: auto;
 
-      font-style: normal;
-      font-weight: normal;
       font-size: 16px;
       line-height: 24px;
-      text-align: center;
-      letter-spacing: -0.02em;
-      opacity: 0.6;
+    }
+    .sponsorBox {
+      right: -75px;
+
+      .sponsorItem {
+        font-size: 14px;
+        line-height: 21px;
+      }
     }
   }
   @include desktop {
@@ -135,6 +232,7 @@ footer {
     margin: auto auto 32px auto;
     .sns {
       display: flex;
+      margin-bottom: 24px;
       img {
         height: 32px;
         width: 32px;
@@ -145,16 +243,25 @@ footer {
         margin-right: 32px;
       }
     }
+    .sponsor {
+      > p {
+        font-size: 16px;
+        line-height: 24px;
+      }
+    }
     .copyrights {
       margin-left: auto;
 
-      font-style: normal;
-      font-weight: normal;
       font-size: 16px;
       line-height: 24px;
-      text-align: center;
-      letter-spacing: -0.02em;
-      opacity: 0.6;
+    }
+    .sponsorBox {
+      right: -75px;
+
+      .sponsorItem {
+        font-size: 14px;
+        line-height: 21px;
+      }
     }
   }
 }
