5ce2e6e9e7852d3a950cb591119adef8f17212f8
mcauto <nexters@kakao.com>
2021-11-01 20:52:20 +0900

    fix: favicon, open-graph.png 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,38 +5,26 @@ const config: NuxtConfig = {
   target: "static",
   ssr: false,
   head: {
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon_N20_03.png" }],
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: "description",
-        name: "description",
-        content:
-          "IT 연합동아리 넥스터즈(NEXTERS)는 IT 생태계의 주인공인 개발자와 디자이너들을 위한 모임입니다. 재능있는 디자이너와 개발자들이 함께 모여 자유로운 분위기에서 어울리고 도움을 주고받으며 협업을 통해 원하는 애플리케이션을 만드는 모임입니다.",
-      },
-      {
         hid: "title",
         name: "title",
-        content: "NEXTERS | 개발자와 디자이너를 위한 IT 연합동아리",
+        content: "NEXTERS : IT Community for Experts",
       },
       {
         hid: "og-image",
         property: "og:image",
         name: "og:image",
-        content: "http://teamnexters.com/img/20th/09_Homepage_favicon.ico",
+        content: "/og-image.png",
       },
       {
         hid: "og-title",
         property: "og:title",
         name: "og:title",
-        content: "NEXTERS | Network for Experts",
-      },
-      {
-        hid: "og-description",
-        property: "og:description",
-        name: "og:description",
-        content:
-          "IT 연합동아리 넥스터즈(NEXTERS)는 IT 생태계의 주인공인 개발자와 디자이너들을 위한 모임입니다. 재능있는 디자이너와 개발자들이 함께 모여 자유로운 분위기에서 어울리고 도움을 주고받으며 협업을 통해 원하는 애플리케이션을 만드는 모임입니다.",
+        content: "NEXTERS : IT Community for Experts",
       },
     ],
   },
