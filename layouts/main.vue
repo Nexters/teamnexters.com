88b8a6e5c0f80932af93eeae8c084a8871533adf
emost22 <emoney96@naver.com>
2023-04-22 15:26:31 +0900

    fix(main): delete log

--- layouts/main.vue
+++ layouts/main.vue
@@ -103,8 +103,6 @@ import background from "~/assets/css/export.scss";
       } else if (this.is_recruiting) {
         img = this.main_wip_desktop;
       }
-
-      console.log('img: ' + img);
       return img;
     },
   },
